63e9516694338b0d17b2d3ef09fa6cb1e4f4f193
Anurag kumar <kumaranurag9795@gmail.com>
2025-09-15 01:29:24 +0530

    Set frontend to use deployed backend

--- frontend/report/src/utils/api.js
+++ frontend/report/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://automatted-attendance-backend.onrender.com/api",
+  baseURL: "https://automatted-attendance-backend.onrender.com",
 });
 
 api.interceptors.request.use((req) => {
